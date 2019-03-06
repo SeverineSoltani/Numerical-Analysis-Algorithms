e904993ad89a5bb35276e71c6d684fa17452ca60
SevSoltani <48298382+SevSoltani@users.noreply.github.com>
2019-03-06 13:43:07 -0800

    Update LUFactorization.m

--- LUFactorization.m
+++ LUFactorization.m
@@ -1,15 +1,19 @@
+% LUFactorization returns the LU factorization with partial pivoting 
+% of A with its lower triangular portion overwritten to be L.
+% LUFactorization also returns flop count.
 function [flopCount, A] = LUFactorization(A,n)
 
 flopCount = 0;
 
 for i = 1:n-1
-    [maxValue, maxValueIndex] = max(abs(A(i:n,i)));
+    [maxValue, maxValueIndex] = max(abs(A(i:n,i))); % finds largest value in col.
     swapThisRow = i - 1 + maxValueIndex;
-    A([swapThisRow, i],:) = A([i, swapThisRow],:);
+    A([swapThisRow, i],:) = A([i, swapThisRow],:); % swaps row with largest col. value with current row
     
     for j = i + 1:n 
-        A(j,i) = A(j,i)/A(i,i);
+        A(j,i) = A(j,i)/A(i,i); % finds multiplier and overwrites in A
         flopCount = flopCount + 1;
+        
         for k = i + 1:n 
             A(j,k) = A(j,k) - A(j,i)*A(i,k);
             flopCount = flopCount + 2;
@@ -18,3 +22,6 @@ for i = 1:n-1
 end
 
 end
+
+% TODO
+% Extension: add permutation matrix
