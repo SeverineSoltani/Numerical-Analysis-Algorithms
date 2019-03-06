449597db87b3caebd7f7eb1680b7b5e91f1c54ca
SevSoltani <48298382+SevSoltani@users.noreply.github.com>
2019-03-06 14:04:56 -0800

    Update PowerMethod.m

--- PowerMethod.m
+++ PowerMethod.m
@@ -1,32 +1,34 @@
+% PowerMethod returns the dominant eigenvector of A, 
+% a matrix in sparse format.
+% PowerMethod also returns flop count.
+
+
 function [lambda, flopcount] = PowerMethod(N,r,c,v,n)
 
-    initGuess = ones(n,1);
+    initGuess = ones(n,1); % initial guess of col. vector of 1s
     
     flopcount = 0;
     m = length(v);
     A = zeros(n,n);
 
     for f = 1:m
-        A(r(f),c(f)) = v(f);
+        A(r(f),c(f)) = v(f); % restores A into full matrix
     end
     
     for i = 1:N
         
-        initGuess = (A*initGuess); 
-        % (A*initGuess) = 32
-        flopcount = flopcount + 32;
+        initGuess = A*initGuess; 
+        flopcount = flopcount + 1;
         initGuessNorm = norm(initGuess);
-        initGuess = initGuess/initGuessNorm; 
+        initGuess = initGuess/initGuessNorm; % normalizes the guess
         flopcount = flopcount + 1;
         
     end
     
     transInitGuess = initGuess';
     lambda = (transInitGuess*(A*initGuess))/(transInitGuess*initGuess);
-    flopcount = flopcount + 49;
+    flopcount = flopcount + 4;
     
-    % (transInitGuess*initGuess) = 8
-    % A*initGuess = 32
-    % (transInitGuess*(A*initGuess) = 8
-    % 32 + 8 + 8 + 1 = 49
 end
+
+% Extension: make a function that takes full matrix A, not just sparse
