63ac03c8df80f7cd3f7e79e8720ffa245ab1dc67
SevSoltani <48298382+SevSoltani@users.noreply.github.com>
2019-03-06 14:12:27 -0800

    Update findInfinityNorm.m

--- findInfinityNorm.m
+++ findInfinityNorm.m
@@ -1,20 +1,19 @@
+% findInfinityNorm finds the infinity of l-norm or n x n matrix A.
 function [infinityNorm] = findInfinityNorm(A,n)
 
-A = inv(A);
-
-sumVec = [];
+sumVec = []; % stores the sum of each row in a col. vector
 for i = 1:n
     sum = 0;
     for j = 1:n
-        sum = abs(A(i,j)) + sum;
+        sum = abs(A(i,j)) + sum; % sums up row of A
         sumVec(i, 1) = sum;
     end
 end
 
 infinityNorm = sumVec(1,1);
 for i = 1:n
-    if sumVec(i,1) > infinityNorm
-        infinityNorm = sumVec(i,1);
+    if sumVec(i,1) > infinityNorm % find greatest value in sumVec
+        infinityNorm = sumVec(i,1); % make the greatest sumVec value the infinityNorm
     end
 end
 
